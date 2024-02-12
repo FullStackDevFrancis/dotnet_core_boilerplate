4c77f8ba757d2c1abfb12d9f7cef2731475223b8
DevFrancis <francis.rousseau.32@gmail.com>
2024-02-12 22:05:51 +0100

    Added docker compose and local docker for hard drive mount

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "tessstssssssss", "test2", "test3"
     };
 
     private readonly ILogger<WeatherForecastController> _logger;
@@ -24,7 +24,7 @@ public class WeatherForecastController : ControllerBase
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
+                TemperatureC = Random.Shared.Next(-300, 5),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
